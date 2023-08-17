0c83d3d0fd5dbb2bcc50aef0448b55cd2cb7a921
gureumwoon <vfrvfr3221@gmail.com>
2023-08-17 18:58:02 +0900

    weather containiner layout 완성

--- App.js
+++ App.js
@@ -6,7 +6,12 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}></View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -27,5 +32,17 @@ const styles = StyleSheet.create({
   },
   weather: {
     flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 168,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   }
 });
